0029d645d43f3a789d5ed1fc12d60e211375c0ae
chenwbj <chenwbj@gmail.com>
2017-08-29 16:32:26 +0800

    remove zikpkin from discovery-service

--- discovery-service/src/main/java/demo/Application.java
+++ discovery-service/src/main/java/demo/Application.java
@@ -3,11 +3,9 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableZipkinServer
 public class Application {
 
 	public static void main(String[] args) {
